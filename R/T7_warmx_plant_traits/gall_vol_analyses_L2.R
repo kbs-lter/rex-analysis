a9e514880bf013ac4f1cd6be621ef85433d0df43
dobsonk2 <65179695+dobsonk2@users.noreply.github.com>
2022-02-02 13:10:33 -0500

    updating gall volume analyses

--- R/T7_warmx_plant_traits/gall_vol_analyses_L2.R
+++ R/T7_warmx_plant_traits/gall_vol_analyses_L2.R
@@ -4,43 +4,54 @@
 # DATA INPUT:     Data imported as csv files from shared REX Google drive T7_warmx_plant_traits L1 folder
 # DATA OUTPUT:    analyses
 # PROJECT:        REX
-# DATE:           July 2021
+# DATE:           July 2021; updated Jan 2022
 
 # Clear all existing data
 rm(list=ls())
 
 # Load packages
-library(tidyverse)
 library(lmerTest)
 library(fitdistrplus)
 library(sjPlot)
 library(tidyverse)
 library(car)
 library(emmeans)
+library(bbmle)
+library(multcomp)
 
 # Set working directory
 dir<-Sys.getenv("DATA_DIR")
 
 # Read in data
-galls <- read.csv(file.path(dir, "T7_warmx_plant_traits/L1/T7_warmx_gall_vol_L1.csv"))
+galls <- read.csv(file.path(dir, "T7_warmx_plant_traits/L1/T7_warmx_Soca_gall_vol_L1.csv"))
 
 # Data exploration
-descdist(galls$sphere_vol, discrete = FALSE)
-hist(galls$sphere_vol)
-qqnorm(galls$sphere_vol)
-shapiro.test(galls$sphere_vol)
-# pretty right skewed, going to try a few transformations
+descdist(galls$sphere_vol_cm3, discrete = FALSE)
+hist(galls$sphere_vol_cm3)
+qqnorm(galls$sphere_vol_cm3)
+shapiro.test(galls$sphere_vol_cm3)
+# pretty right skewed, going to try a few transformations & gamma distribution
 
 # square root transformation
-galls$sqrt_vol <- sqrt(galls$sphere_vol)
+galls$sqrt_vol <- sqrt(galls$sphere_vol_cm3)
 descdist(galls$sqrt_vol, discrete = FALSE)
 hist(galls$sqrt_vol)
 qqnorm(galls$sqrt_vol)
 shapiro.test(galls$sqrt_vol)
-# pretty good
 
-# Assumption checking
-m1 <- lmer(sqrt_vol ~ treatment + (1|rep), data = galls, REML=FALSE)
+# log transformation
+galls$log_vol <- log(galls$sphere_vol_cm3)
+descdist(galls$log_vol, discrete = FALSE)
+hist(galls$log_vol)
+qqnorm(galls$log_vol)
+shapiro.test(galls$log_vol) # slightly better than sqrt
+
+# gamma distribution?
+fit.gamma <- fitdist(galls$sphere_vol_cm3, "gamma")
+plot(fit.gamma) # this looks pretty good
+
+# Assumption checking - log transformation
+m1 <- lmer(log_vol ~ treatment + drought_period + (1|rep), data = galls, REML=FALSE)
 # Check Assumptions:
 # (1) Linearity: if covariates are not categorical
 # (2) Homogeneity: Need to Check by plotting residuals vs predicted values.
@@ -48,16 +59,104 @@ plot(m1, main = "Gall volume")
 # Homogeneity of variance is ok here (increasing variance in resids is not increasing with fitted values)
 # Check for homogeneity of variances (true if p>0.05). If the result is not significant, the assumption of equal variances (homoscedasticity) is met (no significant difference between the group variances).
 leveneTest(residuals(m1) ~ galls$treatment)
+leveneTest(residuals(m1) ~ galls$drought_period)
 # Assumption met
 # (3) Normality of error term: need to check by histogram, QQplot of residuals, could do Kolmogorov-Smirnov test.
 # Check for normal residuals
 qqPlot(resid(m1), main = "Gall volume")
 hist(residuals(m1), main = "Gall volume")
-shapiro.test(resid(m1)) # Good
+shapiro.test(resid(m1))
+outlierTest(m1)
+
+# Assumption checking - sqrt transformation
+m2 <- lmer(sqrt_vol ~ treatment + drought_period + (1|rep), data = galls, REML=FALSE)
+# Check Assumptions:
+# (1) Linearity: if covariates are not categorical
+# (2) Homogeneity: Need to Check by plotting residuals vs predicted values.
+plot(m2, main = "Gall volume")
+# Homogeneity of variance is ok here (increasing variance in resids is not increasing with fitted values)
+# Check for homogeneity of variances (true if p>0.05). If the result is not significant, the assumption of equal variances (homoscedasticity) is met (no significant difference between the group variances).
+leveneTest(residuals(m2) ~ galls$treatment)
+# Assumption met
+# (3) Normality of error term: need to check by histogram, QQplot of residuals, could do Kolmogorov-Smirnov test.
+# Check for normal residuals
+qqPlot(resid(m2), main = "Gall volume")
+hist(residuals(m2), main = "Gall volume")
+shapiro.test(resid(m2))
+outlierTest(m2)
+
+# Assumption checking - gamma distribution (do these apply to non-normal distributions?)
+m3 <- glmer(sphere_vol_cm3 ~ treatment + drought_period + (1|rep), data = galls, family = Gamma)
+plot(m3, main = "Gall volume")
+leveneTest(residuals(m3) ~ galls$treatment)
+leveneTest(residuals(m3) ~ galls$drought_period)
+qqPlot(resid(m3), main = "Gall volume")
+hist(residuals(m3), main = "Gall volume")
+shapiro.test(resid(m3)) # gonna go with gamma distribution
 
 # comparison with other models
-m2 <- lmer(sqrt_vol ~ treatment + (1|rep/footprint), data = galls, REML=FALSE)
-AICctab(m1, m2, weights=T) # going with model 2 because of nested design
+m4 <- glmer(sphere_vol_cm3 ~ treatment + drought_period + (1|rep/footprint), data = galls, family = Gamma)
+m5 <- glmer(sphere_vol_cm3 ~ treatment + (1|drought_period) + (1|rep/footprint), data = galls, family = Gamma)
+m6 <- glmer(sphere_vol_cm3 ~ treatment * drought_period + (1|rep/footprint), data = galls, family = Gamma)
+AICctab(m3, m4, m5, m6, weights=T) # model 4
 
-summary(m2)
-emmeans(m2, list(pairwise ~ treatment), adjust = "tukey")
+summary(m4)
+emmeans(m4, list(pairwise ~ treatment), adjust = "tukey")
+summary(glht(m4, mcp(treatment="Tukey")), test = adjusted(type = "bonferroni"))
+
+# re-leveling the data & checking summary since post-hoc gives diff results (?)
+galls <- within(galls, treatment <- relevel(factor(treatment), ref = "Ambient Drought"))
+m4 <- glmer(sphere_vol_cm3 ~ treatment + drought_period + (1|rep/footprint), data = galls, family = Gamma)
+summary(m4)
+galls <- within(galls, treatment <- relevel(factor(treatment), ref = "Irrigated Control"))
+m4 <- glmer(sphere_vol_cm3 ~ treatment + drought_period + (1|rep/footprint), data = galls, family = Gamma)
+summary(m4)
+galls <- within(galls, treatment <- relevel(factor(treatment), ref = "Warm"))
+m4 <- glmer(sphere_vol_cm3 ~ treatment + drought_period + (1|rep/footprint), data = galls, family = Gamma)
+summary(m4)
+galls <- within(galls, treatment <- relevel(factor(treatment), ref = "Warm Drought"))
+m4 <- glmer(sphere_vol_cm3 ~ treatment + drought_period + (1|rep/footprint), data = galls, family = Gamma)
+summary(m4)
+galls <- within(galls, treatment <- relevel(factor(treatment), ref = "Ambient"))
+m4 <- glmer(sphere_vol_cm3 ~ treatment + drought_period + (1|rep/footprint), data = galls, family = Gamma)
+summary(m4)
+
+galls <- within(galls, drought_period <- relevel(factor(drought_period), ref = "Pre-Drought"))
+m4 <- glmer(sphere_vol_cm3 ~ treatment + drought_period + (1|rep/footprint), data = galls, family = Gamma)
+summary(m4)
+
+# calculating effect size accounting for inverse link
+1/(0.025608 + 0.018283*0) # 39.0503 - average for irrigated control
+1/(0.025608 + 0.018283*1) # 22.78371 - average for warmed
+# effect:
+22.78371 - 39.0503 # 16.26659% smaller galls in warmed compared to irrigated
+
+# calculating effect size accounting for inverse link
+1/(0.043890  + -0.016302*0) # 22.78423 - average for warmed
+1/(0.043890 + -0.016302*1) # 36.24764 - average for drought
+# effect:
+36.24764 - 22.78423 # 13.46341% smaller galls in warmed compared to drought
+
+# calculating effect size accounting for inverse link
+1/(0.043890  + -0.013871*0) # 22.78423 - average for warmed
+1/(0.043890 + -0.013871*1) # 33.31224 - average for warmed drought
+# effect:
+33.31224 - 22.78423 # 10.52801% smaller galls in warmed compared to warmed drought
+
+# calculating effect size accounting for inverse link
+1/(0.043890  + 0.004594*0) # 22.78423 - average for drought
+1/(0.043890 + 0.004594*1) # 20.62536 - average for post-drought
+# effect:
+20.62536 - 22.78423 # 2.15887% smaller galls in post-drought compared to drought
+
+# calculating effect size accounting for inverse link
+1/(0.043890  + 0.014814*0) # 22.78423 - average for drought
+1/(0.043890 + 0.014814*1) # 17.03461 - average for pre-drought
+# effect:
+17.03461 - 22.78423 # 5.74962% smaller galls in pre-drought compared to drought
+
+# calculating effect size accounting for inverse link
+1/(0.058703  + -0.010220*0) # 17.0349 - average for pre-drought
+1/(0.058703 + -0.010220*1) # 20.62579 - average for post-drought
+# effect:
+20.62579 - 17.0349 # 3.59089% smaller galls in pre-drought compared to post-drought
--- R/T7_warmx_plant_traits/gall_vol_plots_L2.R
+++ R/T7_warmx_plant_traits/gall_vol_plots_L2.R
@@ -4,56 +4,159 @@
 # DATA INPUT:     Data imported as csv files from shared REX Google drive T7_warmx_plant_traits L1 folder
 # DATA OUTPUT:    Figures
 # PROJECT:        REX
-# DATE:           July 2021
+# DATE:           July 2021; updated Jan 2022
 
 # Clear all existing data
 rm(list=ls())
 
 # Load packages
 library(tidyverse)
-
-# Set working directory
-dir<-Sys.getenv("DATA_DIR")
-
-# Read in data
-galls <- read.csv(file.path(dir, "T7_warmx_plant_traits/L1/T7_warmx_gall_vol_L1.csv"))
-
-# Take subplot average of galls
-gall_avg <- galls %>%
-  group_by(rep, footprint, treatment) %>%
-  summarize(avg_gall = mean(sphere_vol, na.rm = TRUE))
+library(plotrix)
 
 # Set ggplot2 plotting
 # This code for ggplot2 sets the theme to mostly black and white 
 # (Arial font, and large font, base size=24)
 theme_set(theme_bw(14))
-theme_update(axis.text.x = element_text(size = 16),
+theme_update(axis.text.x = element_text(size = 12),
              axis.text.y = element_text(size = 16),
              axis.title = element_text(size=16,face="bold"))
 
+# Set working directory
+dir<-Sys.getenv("DATA_DIR")
+
+# Read in data
+galls <- read.csv(file.path(dir, "T7_warmx_plant_traits/L1/T7_warmx_Soca_gall_vol_L1.csv"))
+
+# Take subplot average of galls
+gall_avg <- galls %>%
+  group_by(treatment, drought_period) %>%
+  summarize(avg_gall = mean(sphere_vol_cm3, na.rm = TRUE),
+            se = std.error(sphere_vol_cm3, na.rm = TRUE))
+gall_avg2 <- galls %>%
+  group_by(treatment) %>%
+  summarize(avg_gall = mean(sphere_vol_cm3, na.rm = TRUE),
+            se = std.error(sphere_vol_cm3, na.rm = TRUE))
+gall_avg3 <- galls %>%
+  group_by(drought_period) %>%
+  summarize(avg_gall = mean(sphere_vol_cm3, na.rm = TRUE),
+            se = std.error(sphere_vol_cm3, na.rm = TRUE))
+
 # Boxplot of volume x treatment
-png("vol_treatment.png", units="in", width=8, height=6, res=300)
-ggplot(gall_avg, aes(x = treatment, y = avg_gall)) +
+gall_avg <- gall_avg %>%
+  mutate(across(drought_period, factor, levels=c("Pre-Drought","Drought","Post-Drought"))) # re-ordering drought period
+png("gall_volume_overall.png", units="in", width=10, height=6, res=300)
+ggplot(galls, aes(x = treatment, y = sphere_vol_cm3)) +
   geom_boxplot(color = "black", outlier.shape = NA, fill = "olivedrab") +
+  facet_grid(.~drought_period) +
   labs(x = "Treatment", y = "Gall Volume (cm^3)") +
   #scale_fill_manual(values = c("olivedrab")) +
-  scale_x_discrete(limits = c("irr_control", "ambient", "drought", "warmed", "warmed_drought"),
-                   labels=c("ambient" = "Ambient",
-                            "drought" = "Drought",
-                            "irr_control" = "Irrigated \n Control",
-                            "warmed" = "Warmed",
-                            "warmed_drought" = "Warmed & \n Drought"),
+  scale_x_discrete(limits = c("Irrigated Control", "Ambient", "Ambient Drought", "Warm", "Warm Drought"),
+                   labels=c("Ambient" = "Ambient",
+                            "Ambient Drought" = "Drought",
+                            "Irrigated Control" = "Irrigated \n Control",
+                            "Warm" = "Warmed",
+                            "Warm Drought" = "Warmed & \n Drought"),
                    guide = guide_axis(n.dodge=2)) +
-  #geom_jitter(shape=16, position=position_jitterdodge(), alpha = 0.6, aes(colour = gall_present)) +
+  ylim(0,100) +
+  geom_jitter(aes(alpha=0.8), color="olivedrab", fill="olivedrab", shape=16, size=2) +
   theme(legend.position = "none")
 dev.off()
 
-# Regression
-lm_gall <- lm(sphere_vol ~ plant_height, data = galls)
-png("vol_height.png", units="in", width=5, height=5, res=300)
-ggplot(galls,aes(plant_height, sphere_vol)) +
-  geom_point(color='olivedrab') + 
-  geom_smooth(method='lm', color="olivedrab") +
-  labs(x = "Plant Height (cm)", y = "Gall Volume (cm^3)")
+# Boxplot of volume w/o drought period
+png("gall_volume_temp.png", units="in", width=8, height=6, res=300)
+ggplot(galls, aes(x = treatment, y = sphere_vol_cm3)) +
+  geom_boxplot(color = "black", outlier.shape = NA, fill = "olivedrab") +
+  labs(x = "Treatment", y = "Gall Volume (cm^3)") +
+  #scale_fill_manual(values = c("olivedrab")) +
+  scale_x_discrete(limits = c("Irrigated Control", "Ambient", "Ambient Drought", "Warm", "Warm Drought"),
+                   labels=c("Ambient" = "Ambient",
+                            "Ambient Drought" = "Drought",
+                            "Irrigated Control" = "Irrigated \n Control",
+                            "Warm" = "Warmed",
+                            "Warm Drought" = "Warmed & \n Drought"),
+                   guide = guide_axis(n.dodge=2)) +
+  ylim(0,100) +
+  geom_jitter(aes(alpha=0.8), color="olivedrab", fill="olivedrab", shape=16, size=2) +
+  theme(legend.position = "none")
 dev.off()
-summary(lm_gall)
+
+# Boxplot of volume only across drought periods
+png("gall_volume_time.png", units="in", width=8, height=6, res=300)
+ggplot(galls, aes(x = drought_period, y = sphere_vol_cm3)) +
+  geom_boxplot(color = "black", outlier.shape = NA, fill = "olivedrab") +
+  labs(x = "Timing of Drought Period", y = "Gall Volume (cm^3)") +
+  #scale_fill_manual(values = c("olivedrab")) +
+  #scale_x_discrete(limits = c("Irrigated Control", "Ambient", "Ambient Drought", "Warm", "Warm Drought"),
+  #                 labels=c("Ambient" = "Ambient",
+  #                          "Ambient Drought" = "Drought",
+  #                          "Irrigated Control" = "Irrigated \n Control",
+  #                          "Warm" = "Warmed",
+  #                          "Warm Drought" = "Warmed & \n Drought"),
+  #                 guide = guide_axis(n.dodge=2)) +
+  ylim(0,100) +
+  geom_jitter(aes(alpha=0.8), color="olivedrab", fill="olivedrab", shape=16, size=2) +
+  theme(legend.position = "none")
+dev.off()
+
+# Barplot of volume x treatment
+png("gall_volume_overall_bar.png", units="in", width=10, height=6, res=300)
+ggplot(gall_avg, aes(x = treatment, y = avg_gall)) +
+  facet_grid(.~drought_period) +
+  geom_bar(position = "identity", stat = "identity", color = "black",fill = "olivedrab") +
+  geom_errorbar(aes(ymin = avg_gall - se, ymax = avg_gall + se), width = 0.2,
+                         position = "identity") +
+  labs(x = "Treatment", y = "Gall Volume (cm^3)") +
+  scale_fill_manual(values = c("#a6bddb", "#fb6a4a")) +
+  scale_x_discrete(limits = c("Irrigated Control", "Ambient", "Ambient Drought", "Warm", "Warm Drought"),
+                   labels=c("Ambient" = "Ambient",
+                            "Ambient Drought" = "Drought",
+                            "Irrigated Control" = "Irrigated \n Control",
+                            "Warm" = "Warmed",
+                            "Warm Drought" = "Warmed & \n Drought"),
+                   guide = guide_axis(n.dodge=2)) +
+  theme(legend.position = "none")
+dev.off()
+
+# Barplot of only temperature treatment
+png("gall_volume_temp_bar.png", units="in", width=8, height=6, res=300)
+ggplot(gall_avg2, aes(x = treatment, y = avg_gall)) +
+  geom_bar(position = "identity", stat = "identity", color = "black",fill = "olivedrab") +
+  geom_errorbar(aes(ymin = avg_gall - se, ymax = avg_gall + se), width = 0.2,
+                position = "identity") +
+  labs(x = "Treatment", y = "Gall Volume (cm^3)") +
+  scale_fill_manual(values = c("#a6bddb", "#fb6a4a")) +
+  scale_x_discrete(limits = c("Irrigated Control", "Ambient", "Ambient Drought", "Warm", "Warm Drought"),
+                   labels=c("Ambient" = "Ambient",
+                            "Ambient Drought" = "Drought",
+                            "Irrigated Control" = "Irrigated \n Control",
+                            "Warm" = "Warmed",
+                            "Warm Drought" = "Warmed & \n Drought")) +
+  theme(legend.position = "none")
+dev.off()
+
+# Barplot of only drought period
+png("gall_volume_time_bar.png", units="in", width=8, height=6, res=300)
+ggplot(gall_avg3, aes(x = drought_period, y = avg_gall)) +
+  geom_bar(position = "identity", stat = "identity", color = "black",fill = "olivedrab") +
+  geom_errorbar(aes(ymin = avg_gall - se, ymax = avg_gall + se), width = 0.2,
+                position = "identity") +
+  labs(x = "Timing of Drought Period", y = "Gall Volume (cm^3)") +
+  scale_fill_manual(values = c("#a6bddb", "#fb6a4a")) +
+  #scale_x_discrete(limits = c("Irrigated Control", "Ambient", "Ambient Drought", "Warm", "Warm Drought"),
+  #                 labels=c("Ambient" = "Ambient",
+  #                          "Ambient Drought" = "Drought",
+  #                          "Irrigated Control" = "Irrigated \n Control",
+  #                          "Warm" = "Warmed",
+  #                          "Warm Drought" = "Warmed & \n Drought")) +
+  theme(legend.position = "none")
+dev.off()
+
+## Regression - data does not contain height anymore
+#lm_gall <- lm(sphere_vol_cm3 ~ plant_height, data = galls)
+#png("vol_height.png", units="in", width=5, height=5, res=300)
+#ggplot(galls,aes(plant_height, sphere_vol)) +
+#  geom_point(color='olivedrab') + 
+#  geom_smooth(method='lm', color="olivedrab") +
+#  labs(x = "Plant Height (cm)", y = "Gall Volume (cm^3)")
+#dev.off()
+#summary(lm_gall)
