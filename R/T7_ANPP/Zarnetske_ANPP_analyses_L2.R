03bbec47b387bab716006e951895e29dd6a46534
dobsonk2 <65179695+dobsonk2@users.noreply.github.com>
2022-07-08 12:12:31 -0400

    anpp analyses updates

--- R/T7_ANPP/Zarnetske_ANPP_analyses_L2.R
+++ R/T7_ANPP/Zarnetske_ANPP_analyses_L2.R
@@ -30,6 +30,7 @@ anpp <- read.csv(file.path(dir, "T7_ANPP/L1/T7_Zarnetske_ANPP_L1.csv"))
 # Main questions for analyses:
 # Does biomass differ between the climate treatment (warmed, drought, warmed+drought, ambient, irr. control)?
 # Does the biomass of native + exotic species differ between these climate treatments?
+# Also comparisons of growth form + species-specific comparisons
 
 # remove climate treatment plots we aren't interested in here (i.e., insecticide plots)
 anpp <- anpp %>%
@@ -68,11 +69,16 @@ leveneTest(residuals(m1) ~ anpp$Species_Code)
 m2 <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + origin + (1|Replicate/Footprint_Location), data=anpp, REML=F)
 m3 <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions * origin + (1|Replicate/Footprint_Location), data=anpp, REML=F)
 m4 <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + Species_Code + (1|Replicate/Footprint_Location), data=anpp, REML=F)
-m5 <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + (1|Species_Code) + (1|Replicate/Footprint_Location), data=anpp, REML=F)
-anova (m2, m3, m4, m5)
-summary(m4)
-emmeans(m3, list(pairwise ~ Subplot_Descriptions * origin), adjust = "tukey")
-emmip(m3, origin ~ Subplot_Descriptions)
+m5 <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions * Species_Code + (1|Replicate/Footprint_Location), data=anpp, REML=F)
+m6 <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + (1|Species_Code) + (1|Replicate/Footprint_Location), data=anpp, REML=F)
+m7 <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + growth_habit + (1|Replicate/Footprint_Location), data=anpp, REML=F)
+m8 <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions * growth_habit + (1|Replicate/Footprint_Location), data=anpp, REML=F)
+m9 <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + MH_rhizomatous_suggestion + (1|Replicate/Footprint_Location), data=anpp, REML=F)
+m10 <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions * MH_rhizomatous_suggestion + (1|Replicate/Footprint_Location), data=anpp, REML=F)
+AICctab(m2, m3, m4, m5, m6, m7, m8, m9, m10)
+summary(m7)
+emmeans(m5, list(pairwise ~ Subplot_Descriptions * Species_Code), adjust = "tukey")
+emmip(m5, Species_Code ~ Subplot_Descriptions)
 
 # re-leveling the dataframe & re-running model for post-hoc comparisions
 anpp <- within(anpp, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "drought"))
@@ -80,18 +86,72 @@ anpp <- within(anpp, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions
 anpp <- within(anpp, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "warmed"))
 anpp <- within(anpp, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "warmed_drought"))
 anpp <- within(anpp, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "ambient"))
-
-# effect sizes
-# W vs A
-exp(0.88092 + -0.94188*0) # 2.413119 - ambient
-exp(0.88092 + -0.94188*1) # 0.9408609 - warmed
-2.413119 - 0.9408609 # 1.472258 more biomass for ambient
+anpp <- within(anpp, growth_habit <- relevel(factor(growth_habit), ref = "Graminoid"))
+anpp <- within(anpp, growth_habit <- relevel(factor(growth_habit), ref = "Forb"))
+anpp <- within(anpp, growth_habit <- relevel(factor(growth_habit), ref = "Vine"))
+anpp <- within(anpp, MH_rhizomatous_suggestion <- relevel(factor(MH_rhizomatous_suggestion), ref = "non_rhizomatous"))
+anpp <- within(anpp, MH_rhizomatous_suggestion <- relevel(factor(MH_rhizomatous_suggestion), ref = "slightly_rhizomatous"))
+anpp <- within(anpp, MH_rhizomatous_suggestion <- relevel(factor(MH_rhizomatous_suggestion), ref = "highly_rhizomatous"))
 
 # quick anova to check 
 two.way <- aov(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions * origin, data = anpp)
 summary(two.way)
 two.way2 <- aov(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + Species_Code, data = anpp)
 summary(two.way2)
+
+
+### species specific models ###
+anpp_spp <- anpp %>%
+  filter(Species_Code == "TRFPR")
+## list of species to test: ACHMI, SOOGR, ASTPI, ERIAN, SOOCA, ASTSA, HYPPE, PHLPR, DACGL, POAPR, AGRRE, POACO, TRFPR
+m_spp <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + (1|Replicate/Footprint_Location), data=anpp_spp, REML=F)
+summary(m_spp)
+# re-leveling the dataframe & re-running model for post-hoc comparisions
+anpp_spp <- within(anpp_spp, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "drought"))
+anpp_spp <- within(anpp_spp, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "irrigated"))
+anpp_spp <- within(anpp_spp, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "warmed"))
+anpp_spp <- within(anpp_spp, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "warmed_drought"))
+anpp_spp <- within(anpp_spp, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "ambient"))
+
+
+### origin specific models ###
+anpp_org <- anpp %>%
+  filter(origin == "Native")
+m_org <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + (1|Replicate/Footprint_Location), data=anpp_org, REML=F)
+summary(m_org)
+# re-leveling the dataframe & re-running model for post-hoc comparisions
+anpp_org <- within(anpp_org, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "drought"))
+anpp_org <- within(anpp_org, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "irrigated"))
+anpp_org <- within(anpp_org, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "warmed"))
+anpp_org <- within(anpp_org, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "warmed_drought"))
+anpp_org <- within(anpp_org, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "ambient"))
+
+
+### growth form specific models ###
+anpp_growth <- anpp %>%
+  filter(growth_habit == "Forb")
+m_growth <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + (1|Replicate/Footprint_Location), data=anpp_growth, REML=F)
+summary(m_growth)
+# re-leveling the dataframe & re-running model for post-hoc comparisions
+anpp_growth <- within(anpp_growth, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "drought"))
+anpp_growth <- within(anpp_growth, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "irrigated"))
+anpp_growth <- within(anpp_growth, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "warmed"))
+anpp_growth <- within(anpp_growth, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "warmed_drought"))
+anpp_growth <- within(anpp_growth, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "ambient"))
+
+
+### rhizomatous specific models ###
+anpp_rhiz <- anpp %>%
+  filter(MH_rhizomatous_suggestion == "highly_rhizomatous")
+m_rhiz <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + (1|Replicate/Footprint_Location), data=anpp_rhiz, REML=F)
+summary(m_rhiz)
+# re-leveling the dataframe & re-running model for post-hoc comparisions
+anpp_rhiz <- within(anpp_rhiz, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "drought"))
+anpp_rhiz <- within(anpp_rhiz, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "irrigated"))
+anpp_rhiz <- within(anpp_rhiz, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "warmed"))
+anpp_rhiz <- within(anpp_rhiz, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "warmed_drought"))
+anpp_rhiz <- within(anpp_rhiz, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "ambient"))
+
 
 # looking at data between treatments
 ggplot(anpp, aes(x=origin, y=Dried_Plant_Biomass_g)) + 
