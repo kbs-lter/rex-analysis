12864fb937895cc95174b6828f0424bd57a2ef4c
Moriah Young <51270455+moriahy@users.noreply.github.com>
2022-07-20 21:53:23 -0400

    add file via upload

---
+++ R/T2/T2_wheat_measurements_plots_L2.R
@@ -0,0 +1,109 @@
+# TITLE:          REX: T2 Wheat Measurements - height, greenness, biomass
+# AUTHORS:        Moriah Young, Lisa Leonard
+# COLLABORATORS:  Gran Falvo (biomass data)
+# DATA INPUT:     Data imported as csv files from shared REX Google drive 
+# DATA OUTPUT:    
+# PROJECT:        REX
+# DATE:           July 2022
+
+# Clear all existing data
+rm(list=ls())
+
+# Load packages
+library(tidyverse)
+library(ggplot2)
+library(plotrix)
+
+# Set working directory from .Renviron
+dir <- Sys.getenv("DATA_DIR")
+list.files(dir)
+
+# Read in data
+#wheat_data <-read.csv("C:\\Users\\lisal\\Downloads\\T2_height_greenness_2022_L0 - Sheet1 (2).csv")
+wheat_data <- read.csv(file.path(dir, "T2_height_greenness_2022_L0.csv")) # Moriah
+#biomass_data<-read.csv("C:\\Users\\lisal\\Downloads\\T2_biomass_2022_L0 - Sheet1.csv")
+biomass_data <- read.csv(file.path(dir, "T2_biomass_2022_L0.csv"))
+
+# look at data
+View(wheat_data)
+str(wheat_data)
+summary(wheat_dat$Greenness)
+summary(wheat_dat$Height_cm)
+
+#taking height avg with %>% and summarizing all the heights used in the datasheet, 
+height_avg <-wheat_data %>% 
+        group_by(Subplot_Descriptions) %>%
+        summarize(height_avg=mean(Height_cm,na.rm=TRUE),
+                  se_height=std.error(Height_cm,na.rm=TRUE))
+
+#now do this for avg_greenness by plugging in greenness. 
+avg_greenness <-wheat_data %>%
+        group_by(Subplot_Descriptions)%>%
+        summarize(avg_greenness=mean(Greenness,na.rm=TRUE),
+                  se_greenness=std.error(Greenness,na.rm=TRUE))
+
+#lastly,take biomass avg and do standard error
+biomass_avg<-biomass_data %>%
+        group_by(Subplot_Descriptions) %>%
+        summarize(biomass_avg=mean(anpp,na.rm=TRUE),
+                  se_biomass=std.error(anpp,na.rm=TRUE))
+
+#merge all three datasets together
+wheat_green_height<-merge.data.frame(height_avg,avg_greenness)
+
+#now merge biomass as well
+wheat_green_height_biomass<-merge(wheat_green_height,biomass_avg)
+
+#Steal Moriah's code from Github,thx
+#Plot for height
+ggplot(wheat_green_height_biomass, aes(x = Subplot_Descriptions, y = height_avg, fill = Subplot_Descriptions)) +
+  geom_jitter(shape=16, position=position_jitterdodge(), alpha = 0.6, aes(colour = Subplot_Descriptions)) +
+  geom_bar(stat = "identity") +
+  geom_errorbar(aes(ymin = height_avg - se_height, ymax = height_avg + se_height), width = 0.2,
+                position = "identity") +
+  scale_fill_manual(values = c("control" = "orchid1", "control_fungicide" = "cyan2", 
+       "drought_control" = "deepskyblue", "drought_corn_control" = "chartreuse1", "drought_fungicide" = "lightsteelblue2",
+       "drought_legacy_control"= "seagreen2", "drought_legacy_fungicide"= "turquoise")) +
+  labs(x = "Treatment", y = "Average height (cm)") +
+  scale_x_discrete(labels=c("control" = "Control",
+                            "control_fungicide" = "Fungicide",
+                          "drought_control" = "Drought",
+                           "drought_corn_control" = "Year 3 \n Drought",
+                           "drought_fungicide" = "Drought \n Fungicide",
+                            "drought_legacy_control"= "Drought \n Legacy",   
+                             "drought_legacy_fungicide"="Drought \n Legacy Fungicide")) +
+  theme_classic() +
+  theme(legend.position = "none")
+  
+#now do this for greenness as well as biomass
+ggplot(wheat_green_height_biomass, aes(x = Subplot_Descriptions, y = avg_greenness, fill = Subplot_Descriptions)) +
+  geom_jitter(shape=16, position=position_jitterdodge(), alpha = 0.6, aes(colour = Subplot_Descriptions)) +
+  geom_bar(stat = "identity") +
+  geom_errorbar(aes(ymin = avg_greenness- se_greenness, ymax = avg_greenness + se_greenness), width = 0.2,
+                position = "identity") +
+  scale_fill_manual(values = c("control" = "orchid1", "control_fungicide" = "cyan2", 
+                               "drought_control" = "deepskyblue", "drought_corn_control" = "lightcoral", "drought_fungicide" = "deeppink",
+                               "drought_legacy_control"= "plum4", "drought_legacy_fungicide"= "blue2")) +
+  labs(x = "Treatment", y = "Average Greenness") +
+  scale_x_discrete(labels=c("control" = "Control",
+                            "control_fungicide" = "Fungicide",
+                            "drought_control" = "Drought",
+                            "drought_corn_control" = "Year 3 \n Drought",
+                            "drought_fungicide" = "Drought \n Fungicide",
+                            "drought_legacy_control"= "Drought \n Legacy",   
+                            "drought_legacy_fungicide"="Drought \n Legacy Fungicide")) +
+  theme_classic()+
+  theme(legend.position="none")
+  #Fungicide=red colors, drought=blue, both purple idk wjnrw;vofr.erw
+
+#lastly, do this for biomass
+ggplot(wheat_green_height_biomass, aes(x = Subplot_Descriptions, y = biomass_avg, fill = Subplot_Descriptions)) +
+  geom_jitter(shape=16, position=position_jitterdodge(), alpha = 0.6, aes(colour = Subplot_Descriptions)) +
+  geom_bar(stat = "identity") +
+  geom_errorbar(aes(ymin = biomass_avg- se_biomass, ymax = biomass_avg + se_biomass), width = 0.2,
+                position = "identity") +
+  labs(x = "Treatment", y = "Average Biomass") +
+  theme_classic()+
+  theme(legend.position="none")
+#Try to do some boxplots for the height, greenness, biomass
+#geom_box?
