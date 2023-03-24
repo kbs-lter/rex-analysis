7e8470f2cc749d3272f1c6fc0cc74e236125dda8
dobsonk2 <65179695+dobsonk2@users.noreply.github.com>
2023-03-24 11:30:51 -0400

    including 2022 data in anpp analyses

--- R/T7_ANPP/Zarnetske_ANPP_analyses_L2.R
+++ R/T7_ANPP/Zarnetske_ANPP_analyses_L2.R
@@ -25,7 +25,18 @@ library(plotrix)
 dir <- Sys.getenv("DATA_DIR")
 
 # Read in data
-anpp <- read.csv(file.path(dir, "T7_ANPP/L1/T7_Zarnetske_ANPP_L1.csv"))
+anpp_2021 <- read.csv(file.path(dir, "T7_ANPP/L1/T7_warmx_ANPP_2021_L1.csv"))
+anpp_2022 <- read.csv(file.path(dir, "T7_ANPP/L1/T7_warmx_ANPP_2022_L1.csv"))
+
+# delete unneeded columns
+anpp_2021 = subset(anpp_2021, select = -c(Replicate, Unique_ID))
+
+# add year column
+anpp_2021$Year <- "2021"
+anpp_2022$Year <- "2022"
+
+# merge both years
+anpp <- rbind(anpp_2021,anpp_2022)
 
 # Main questions for analyses:
 # Does biomass differ between the climate treatment (warmed, drought, warmed+drought, ambient, irr. control)?
@@ -33,20 +44,12 @@ anpp <- read.csv(file.path(dir, "T7_ANPP/L1/T7_Zarnetske_ANPP_L1.csv"))
 # Also comparisons of growth form + species-specific comparisons
 
 # remove climate treatment plots we aren't interested in here (i.e., insecticide plots)
+unique(anpp$Subplot_Descriptions)
 anpp <- anpp %>%
   filter(!(Subplot_Descriptions=='drought_insecticide' |
              Subplot_Descriptions=='insecticide' |
              Subplot_Descriptions=='warmed_drought_insecticide' |
              Subplot_Descriptions=='warmed_insecticide'))
-
-# remove species we aren't interested in (i.e., unknowns and unsorted plant material)
-anpp <- anpp %>%
-  filter(!(Species_Code=='UNASTAE' |
-             Species_Code=='UNDIC' |
-             Species_Code=='UNFABAE' |
-             Species_Code=='UNSRT' |
-             Species_Code=='CORSPP' | # removing cornus species here (for now) bc it is not designated as native/exotic
-             origin == 'Both')) # removing species listed as both native and exotic
 
 
 #### Data exploration ###
@@ -56,64 +59,106 @@ qqnorm(anpp$Dried_Plant_Biomass_g)
 
 # checking model with log transformation (untransformed data was right skewed)
 # is it normally distributed? yes
-m1 <- lm(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + Species_Code, data=anpp)
+m1 <- lm(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + Species_Code + Year, data=anpp)
 hist(resid(m1))
 qqnorm(resid(m1))
-plot(m1)
-# homogeneity of variance? yes, p >0.05 (no significant difference between the group variances)
-leveneTest(residuals(m1) ~ anpp$Subplot_Descriptions)
-leveneTest(residuals(m1) ~ anpp$Species_Code)
+shapiro.test(resid(m1))
+# homogeneity of variance? yes if p >0.05 (no significant difference between the group variances)
+leveneTest(residuals(m1) ~ anpp$Subplot_Descriptions) # yes
+leveneTest(residuals(m1) ~ anpp$Species_Code) # no, but not surprising that species differ
+leveneTest(residuals(m1) ~ anpp$Year) # yes
+
+# checking model with sqrt transformation (untransformed data was right skewed)
+# is it normally distributed? yes
+m1.2 <- lm(sqrt(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + Species_Code + Year, data=anpp)
+hist(resid(m1.2))
+qqnorm(resid(m1.2))
+shapiro.test(resid(m1.2))
+# homogeneity of variance? yes if p >0.05 (no significant difference between the group variances)
+leveneTest(residuals(m1.2) ~ anpp$Subplot_Descriptions) # yes
+leveneTest(residuals(m1.2) ~ anpp$Species_Code) # no
+leveneTest(residuals(m1.2) ~ anpp$Year) # yes
+
 
 
 ### Model exploration ###
-m2 <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + origin + (1|Replicate/Footprint_Location), data=anpp, REML=F)
-m3 <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions * origin + (1|Replicate/Footprint_Location), data=anpp, REML=F)
-m4 <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + Species_Code + (1|Replicate/Footprint_Location), data=anpp, REML=F)
-m5 <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions * Species_Code + (1|Replicate/Footprint_Location), data=anpp, REML=F)
-m6 <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + (1|Species_Code) + (1|Replicate/Footprint_Location), data=anpp, REML=F)
-m7 <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + growth_habit + (1|Replicate/Footprint_Location), data=anpp, REML=F)
-m8 <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions * growth_habit + (1|Replicate/Footprint_Location), data=anpp, REML=F)
-m9 <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + MH_rhizomatous_suggestion + (1|Replicate/Footprint_Location), data=anpp, REML=F)
-m10 <- lmer(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions * MH_rhizomatous_suggestion + (1|Replicate/Footprint_Location), data=anpp, REML=F)
-AICctab(m2, m3, m4, m5, m6, m7, m8, m9, m10)
+## species-level biomass (plot-summed biomass models down below, and are likely more useful)
+# year
+m2 <- lmer(sqrt(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + Year + (1|Rep/Footprint_Location), data=anpp, REML=F)
+m3 <- lmer(sqrt(Dried_Plant_Biomass_g) ~ Subplot_Descriptions * Year + (1|Rep/Footprint_Location), data=anpp, REML=F)
+anova(m2,m3)
+AICctab(m2,m3) # m2
+anova(m2)
+summary(m2)
+# origin
+m4 <- lmer(sqrt(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + origin + Year + (1|Rep/Footprint_Location), data=anpp, REML=F)
+m5 <- lmer(sqrt(Dried_Plant_Biomass_g) ~ Subplot_Descriptions * origin + Year + (1|Rep/Footprint_Location), data=anpp, REML=F)
+anova(m4,m5)
+AICctab(m4,m5) # m4
+anova(m4)
 summary(m4)
-emmeans(m5, list(pairwise ~ Subplot_Descriptions * Species_Code), adjust = "tukey")
-emmip(m5, Species_Code ~ Subplot_Descriptions)
+# species
+m6 <- lmer(sqrt(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + Species_Code + Year + (1|Rep/Footprint_Location), data=anpp, REML=F)
+m7 <- lmer(sqrt(Dried_Plant_Biomass_g) ~ Subplot_Descriptions * Species_Code + Year + (1|Rep/Footprint_Location), data=anpp, REML=F)
+anova(m6,m7)
+AICctab(m6,m7) # going with m7
+anova(m7)
+summary(m7)
+# growth habit
+m8 <- lmer(sqrt(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + growth_habit + Year + (1|Rep/Footprint_Location), data=anpp, REML=F)
+m9 <- lmer(sqrt(Dried_Plant_Biomass_g) ~ Subplot_Descriptions * growth_habit + Year + (1|Rep/Footprint_Location), data=anpp, REML=F)
+anova(m8,m9)
+AICctab(m8,m9) # m8
+anova(m8)
+summary(m8)
+# rhizomatous
+m10 <- lmer(sqrt(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + MH_rhizomatous_suggestion + Year + (1|Rep/Footprint_Location), data=anpp, REML=F)
+m11 <- lmer(sqrt(Dried_Plant_Biomass_g) ~ Subplot_Descriptions * MH_rhizomatous_suggestion + Year + (1|Rep/Footprint_Location), data=anpp, REML=F)
+anova(m10,m11)
+AICctab(m10,m11) # m10
+anova(m10)
+summary(m10)
 
-# re-leveling the dataframe & re-running model for post-hoc comparisions
-anpp <- within(anpp, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "drought"))
-anpp <- within(anpp, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "irrigated"))
-anpp <- within(anpp, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "warmed"))
-anpp <- within(anpp, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "warmed_drought"))
-anpp <- within(anpp, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "ambient"))
-anpp <- within(anpp, growth_habit <- relevel(factor(growth_habit), ref = "Graminoid"))
-anpp <- within(anpp, growth_habit <- relevel(factor(growth_habit), ref = "Forb"))
-anpp <- within(anpp, growth_habit <- relevel(factor(growth_habit), ref = "Vine"))
-anpp <- within(anpp, MH_rhizomatous_suggestion <- relevel(factor(MH_rhizomatous_suggestion), ref = "non_rhizomatous"))
-anpp <- within(anpp, MH_rhizomatous_suggestion <- relevel(factor(MH_rhizomatous_suggestion), ref = "slightly_rhizomatous"))
-anpp <- within(anpp, MH_rhizomatous_suggestion <- relevel(factor(MH_rhizomatous_suggestion), ref = "highly_rhizomatous"))
-
-# quick anova to check 
-two.way <- aov(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions * origin, data = anpp)
-summary(two.way)
-two.way2 <- aov(log(Dried_Plant_Biomass_g) ~ Subplot_Descriptions + Species_Code, data = anpp)
-summary(two.way2)
 
 # summing data to plot-level for analyses
 anpp_plot <- anpp %>%
-  group_by(Treatment, Replicate, Footprint_Location, Subplot_Location, Subplot_Descriptions) %>% 
+  group_by(Rep, Footprint_Location, Subplot_Location, Subplot_Descriptions, Year) %>% 
   summarize(sum_biomass = sum(Dried_Plant_Biomass_g, na.rm = TRUE))
-m_plot1 <- lmer(sum_biomass ~ Subplot_Descriptions + (1|Replicate/Footprint_Location), data=anpp_plot, REML=F)
-summary(m_plot1)
-anpp_plot <- within(anpp_plot, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "irrigated"))
-
-anpp_plot_growth <- anpp %>%
-  group_by(Treatment, Replicate, Footprint_Location, Subplot_Location, Subplot_Descriptions, growth_habit) %>% 
-  summarize(sum_biomass = sum(Dried_Plant_Biomass_g, na.rm = TRUE)) %>%
-  filter(!(growth_habit == "Vine"))
-m_plot2 <- lmer(sum_biomass ~ growth_habit + (1|Replicate/Footprint_Location), data=anpp_plot_growth, REML=F)
+hist(anpp_plot$sum_biomass)
+# overall biomass
+m_plot1 <- lmer(sum_biomass ~ Subplot_Descriptions + (1|Rep/Footprint_Location), data=anpp_plot, REML=F)
+m_plot2 <- lmer(sum_biomass ~ Subplot_Descriptions + Year + (1|Rep/Footprint_Location), data=anpp_plot, REML=F)
+m_plot3 <- lmer(sum_biomass ~ Subplot_Descriptions * Year + (1|Rep/Footprint_Location), data=anpp_plot, REML=F)
+anova(m_plot1,m_plot2)
+anova(m_plot2,m_plot3)
+hist(resid(m_plot2))
+shapiro.test(resid(m_plot2))
+anova(m_plot2)
 summary(m_plot2)
-anpp_plot <- within(anpp_plot, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "warmed_drought"))
+anpp_plot <- within(anpp_plot, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "warmed"))
+# one year
+anpp_plot_yr <- anpp_plot %>%
+  filter(Year == "2021") # changed to the year I wanted & re-ran the model each time
+m_plot_yr <- lmer(sum_biomass ~ Subplot_Descriptions + (1|Rep/Footprint_Location), data=anpp_plot_yr, REML=F)
+hist(resid(m_plot_yr))
+shapiro.test(resid(m_plot_yr))
+anova(m_plot_yr)
+summary(m_plot_yr)
+anpp_plot_yr <- within(anpp_plot_yr, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "irrigated_control"))
+
+
+# growth habit
+anpp_plot_growth <- anpp %>%
+  group_by(Rep, Footprint_Location, Subplot_Location, Subplot_Descriptions, growth_habit, Year) %>% 
+  summarize(sum_biomass = sum(Dried_Plant_Biomass_g, na.rm = TRUE)) %>%
+  filter(!(growth_habit == "Vine" | growth_habit == "" | growth_habit == "Shurb" | growth_habit == "Subshrub/vine"))
+# one year
+anpp_plot_growth_yr <- anpp_plot_growth %>%
+  filter(Year == "2022" & growth_habit == "Forb") # changed to the year I wanted & re-ran the model each time
+m_plot_growth2 <- lmer(sum_biomass ~ Subplot_Descriptions + (1|Rep/Footprint_Location), data=anpp_plot_growth_yr, REML=F)
+anova(m_plot_growth2)
+summary(m_plot_growth2)
+anpp_plot_growth_yr <- within(anpp_plot_growth_yr, Subplot_Descriptions <- relevel(factor(Subplot_Descriptions), ref = "irrigated_control"))
 
 
 ### species specific models ###
