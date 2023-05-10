9608c7bbdfa33c6f428c448789d22818cc277ab2
dobsonk2 <65179695+dobsonk2@users.noreply.github.com>
2023-05-10 14:14:26 -0400

    updating air temp plots

--- R/T7_warmx_HOBO/T7_warmx_HOBO_plots_L2.R
+++ R/T7_warmx_HOBO/T7_warmx_HOBO_plots_L2.R
@@ -19,7 +19,7 @@ library(tidyverse)
 library(plotrix)
 
 # Read in data
-hobo_data <- read.csv(file.path(dir, "sensors/Phoebe Footprints/L1/T7_warmx_HOBO_L1.csv"))
+hobo_data <- read.csv(file.path(dir, "sensors/OTC Footprints/L1/T7_warmx_HOBO_L1.csv"))
 str(hobo_data)
 # re-making the data column a date
 hobo_data$Date_Time <- as.POSIXct(hobo_data$Date_Time, format = "%Y-%m-%d %H:%M:%S")
@@ -60,6 +60,22 @@ hobo_monthly_avg <- hobo_monthly %>%
   group_by(month, Treatment) %>%
   summarize(avg_temp = mean(average_temp, na.rm = TRUE),
             se = std.error(average_temp, na.rm = TRUE))
+
+# taking monthly average - 2022 only & only may-sep
+# create new dataframes for temperatures averaged for plotting
+hobo_monthly_2022 <- hobo_season %>%
+  filter(year == 2022) %>%
+  #filter(month > "04") %>%
+  #filter(month < "10") %>%
+  group_by(month, Rep, Treatment) %>%
+  summarize(average_temp = mean(Temperature_C, na.rm = TRUE),
+            se = std.error(Temperature_C, na.rm = TRUE))
+# averaging over each rep, so n=6 for each data point (n=5 for D and WD)
+hobo_monthly_avg_2022 <- hobo_monthly_2022 %>%
+  group_by(month, Treatment) %>%
+  summarize(avg_temp = mean(average_temp, na.rm = TRUE),
+            se = std.error(average_temp, na.rm = TRUE))
+print(hobo_monthly_avg_2022)
 
 # taking monthly average
 # create new dataframes for lux averaged for plotting
@@ -140,8 +156,8 @@ hobo_daily_diff <- hobo_daily_diff %>%
 
 
 # plot - monthly
-png("rex_hobo.png", units="in", width=6, height=4, res=300)
-ggplot(hobo_monthly_avg, aes(x = month, y = avg_temp, group=Treatment, color = Treatment)) +
+png("rex_hobo.png", units="in", width=7, height=4, res=300)
+ggplot(hobo_monthly_avg_2022, aes(x = month, y = avg_temp, group=Treatment, color = Treatment)) +
   geom_errorbar(aes(ymin=avg_temp-se, ymax=avg_temp+se),width=0.1,color="black",linetype="solid") +
   geom_line(size = 1) +
   geom_point(size = 2) +
@@ -149,7 +165,11 @@ ggplot(hobo_monthly_avg, aes(x = month, y = avg_temp, group=Treatment, color = T
                      values = c("#a6bddb", "#687689", "#fb6a4a", "#9D422E"),
                      labels=c("Ambient","Drought","Warmed", "Warmed Drought")) +
   labs(y="Temperature (°C)", x="Month") +
-  theme_classic()
+  theme_bw() +
+  theme(axis.title = element_text(size=17),
+        axis.text = element_text(size=15),
+        legend.title = element_text(size=17),
+        legend.text = element_text(size=15))
 dev.off()
 
 # plot - monthly lux
