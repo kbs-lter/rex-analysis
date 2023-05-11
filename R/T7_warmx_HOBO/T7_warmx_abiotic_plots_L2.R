03f9b8072b78a8ec8408ea16db01071422699a6e
dobsonk2 <65179695+dobsonk2@users.noreply.github.com>
2023-05-11 11:27:44 -0400

    updating rex temp + moisture figure

--- R/T7_warmx_HOBO/T7_warmx_abiotic_plots_L2.R
+++ R/T7_warmx_HOBO/T7_warmx_abiotic_plots_L2.R
@@ -1,4 +1,4 @@
-# TITLE:          REX: HOBO plots
+# TITLE:          REX: Abiotic plots
 # AUTHORS:        Kara Dobson
 # COLLABORATORS:  Phoebe Zarnetske, Mark Hammond, Moriah Young, Emily Parker
 # DATA INPUT:     Data imported as csv files from shared REX Google drive sensor data L1 folder
@@ -17,12 +17,17 @@ list.files(dir)
 # Load packages
 library(tidyverse)
 library(plotrix)
+library(ggpubr)
 
 # Read in data
 hobo_data <- read.csv(file.path(dir, "sensors/OTC Footprints/L1/T7_warmx_HOBO_L1.csv"))
+soil_data <- read.csv(file.path(dir, "sensors/OTC Footprints/L1/T7_warmx_soil_sensors_L1.csv"))
 str(hobo_data)
+str(soil_data)
 # re-making the data column a date
 hobo_data$Date_Time <- as.POSIXct(hobo_data$Date_Time, format = "%Y-%m-%d %H:%M:%S")
+soil_data$Date_Time <- as.POSIXct(soil_data$sample_datetime, format = "%Y-%m-%d %H:%M:%S")
+
 
 # create new dataframe with only data from april - august from 7 AM - 7 PM (growing season during the day)
 # this is mainly what we'll work with for figures (could always adjust the parameters as needed)
@@ -31,11 +36,24 @@ hobo_season$month <- format(hobo_season$Date_Time,format="%m")
 hobo_season$year <- format(hobo_season$Date_Time,format="%Y")
 hobo_season$hour <- format(hobo_season$Date_Time, format="%H")
 hobo_season$day <- format(hobo_season$Date_Time, format="%d")
-
 hobo_season <- hobo_season %>%
   filter(hour > "06") %>%
   filter(hour < "20")
 hobo_season_sum <- hobo_season %>%
+  filter(hour > "06") %>%
+  filter(hour < "20") %>%
+  filter(month > "05") %>%
+  filter(month < "09")
+
+soil_season <- soil_data
+soil_season$month <- format(soil_season$Date_Time,format="%m")
+soil_season$year <- format(soil_season$Date_Time,format="%Y")
+soil_season$hour <- format(soil_season$Date_Time, format="%H")
+soil_season$day <- format(soil_season$Date_Time, format="%d")
+soil_season <- soil_season %>%
+  filter(hour > "06") %>%
+  filter(hour < "20")
+soil_season_sum <- soil_season %>%
   filter(hour > "06") %>%
   filter(hour < "20") %>%
   filter(month > "05") %>%
@@ -61,29 +79,8 @@ hobo_monthly_avg <- hobo_monthly %>%
   summarize(avg_temp = mean(average_temp, na.rm = TRUE),
             se = std.error(average_temp, na.rm = TRUE))
 
-# taking 2022 average from june 1 - july 15
-hobo_season$date <- paste0(hobo_season$month,"",hobo_season$day)
-hobo_season$date <- as.numeric(hobo_season$date)
-# selecting 2022 and june 1 - july 15
-hobo_2022 <- hobo_season %>%
-  filter(year == 2022) %>%
-  filter(date > "531") %>%
-  filter(date < "716")
-# limit to the reps I used
-hobo_2022 <- hobo_2022 %>%
-  filter(Rep == 2 | Rep == 3 | Rep == 4 | Rep == 5)
-# take average per day, per rep, per treatment
-hobo_2022_avg <- hobo_2022 %>%
-  group_by(Rep, Treatment) %>%
-  summarize(average_temp = mean(Temperature_C, na.rm = TRUE))
-# averaging over each rep so n=4 for each treatment
-hobo_2022_avg <- hobo_2022_avg %>%
-  group_by(Treatment) %>%
-  summarize(avg_temp = mean(average_temp, na.rm = TRUE),
-            se = std.error(average_temp, na.rm = TRUE))
 
-
-# taking 2022 average for july 11 - july 15 (sampling period)
+# taking 2022 temp average for july 11 - july 15 (sampling period)
 hobo_season$date <- paste0(hobo_season$month,"",hobo_season$day)
 hobo_season$date <- as.numeric(hobo_season$date)
 # selecting 2022 and june 1 - july 15
@@ -103,6 +100,30 @@ hobo_sampling_avg <- hobo_sampling_avg %>%
   group_by(Treatment) %>%
   summarize(avg_temp = mean(average_temp, na.rm = TRUE),
             se = std.error(average_temp, na.rm = TRUE))
+
+# taking 2022 soil average for july 11 - july 15 (sampling period)
+soil_season$date <- paste0(soil_season$month,"",soil_season$day)
+soil_season$date <- as.numeric(soil_season$date)
+# selecting 2022 and june 1 - july 15
+soil_sampling <- soil_season %>%
+  filter(year == 2022) %>%
+  filter(date > "710") %>%
+  filter(date < "716")
+# limit to the reps I used
+soil_sampling <- soil_sampling %>%
+  filter(Rep == 2 | Rep == 3 | Rep == 4 | Rep == 5)
+# take average per day, per rep, per treatment
+soil_sampling_avg <- soil_sampling %>%
+  group_by(Rep, Subplot_Descriptions) %>%
+  summarize(average_temp = mean(temperature, na.rm = TRUE),
+            average_moist = mean(vwc, na.rm=T))
+# averaging over each rep, n varies between treatments
+soil_sampling_avg <- soil_sampling_avg %>%
+  group_by(Subplot_Descriptions) %>%
+  summarize(avg_temp = mean(average_temp, na.rm = TRUE),
+            se_temp = std.error(average_temp, na.rm = TRUE),
+            avg_moist = mean(average_moist, na.rm=T),
+            se_moist = std.error(average_moist, na.rm=T))
 
 
 # taking monthly average
@@ -198,6 +219,56 @@ ggplot(hobo_monthly_avg_2022, aes(x = month, y = avg_temp, group=Treatment, colo
         axis.text = element_text(size=15),
         legend.title = element_text(size=17),
         legend.text = element_text(size=15))
+dev.off()
+
+# plot - air temp, soil temp, and soil moisture during sampling period
+level_order1 <- c("Ambient", 'Warmed', 'Drought',"Warmed_Drought") 
+level_order2 <- c("irrigated_control","ambient", 'warmed', 'drought',"warmed_drought") 
+air_temp <- ggplot(hobo_sampling_avg, aes(x = factor(Treatment, level = level_order1), y = avg_temp)) +
+  geom_pointrange(aes(ymin=avg_temp-se, ymax=avg_temp+se), size=0.6) +
+  #geom_errorbar(aes(ymin=avg_temp-se, ymax=avg_temp+se),width=0.1,color="black",linetype="solid") +
+  #geom_point(size = 2) +
+  labs(y="Air temperature (°C)", x=NULL) +
+  scale_x_discrete(labels=c("Ambient" = "A", "Drought" = "D",
+                            "Warmed" = "W",
+                            "Warmed_Drought" = "WD")) +
+  theme_bw() +
+  theme(axis.title = element_text(size=17),
+        axis.text = element_text(size=15),
+        legend.title = element_text(size=17),
+        legend.text = element_text(size=15))
+soil_temp <- ggplot(soil_sampling_avg, aes(x = factor(Subplot_Descriptions, level=level_order2), y = avg_temp)) +
+  geom_pointrange(aes(ymin=avg_temp-se_temp, ymax=avg_temp+se_temp), size=0.6) +
+  #geom_errorbar(aes(ymin=avg_temp-se_temp, ymax=avg_temp+se_temp),width=0.1,color="black",linetype="solid") +
+  #geom_point(size = 2) +
+  labs(y="Soil temperature (°C)", x=NULL) +
+  scale_x_discrete(labels=c("ambient" = "A", "drought" = "D",
+                            "irrigated_control" = "I", "warmed" = "W",
+                            "warmed_drought" = "WD")) +
+  theme_bw() +
+  theme(axis.title = element_text(size=17),
+        axis.text = element_text(size=15),
+        legend.title = element_text(size=17),
+        legend.text = element_text(size=15))
+soil_moist <- ggplot(soil_sampling_avg, aes(x = factor(Subplot_Descriptions, level=level_order2), y = avg_moist)) +
+  geom_pointrange(aes(ymin=avg_moist-se_moist, ymax=avg_moist+se_moist),size=0.6) +
+  #geom_errorbar(aes(ymin=avg_moist-se_moist, ymax=avg_moist+se_moist),width=0.1,color="black",linetype="solid") +
+  #geom_point(size = 2) +
+  labs(y="Soil moisture (m^3/m^3)", x=NULL) +
+  scale_x_discrete(labels=c("ambient" = "A", "drought" = "D",
+                            "irrigated_control" = "I", "warmed" = "W",
+                            "warmed_drought" = "WD")) +
+  theme_bw() +
+  theme(axis.title = element_text(size=17),
+        axis.text = element_text(size=15),
+        legend.title = element_text(size=17),
+        legend.text = element_text(size=15))
+# combine plots
+abiotic_comb <- ggpubr::ggarrange(air_temp,soil_temp,soil_moist,
+                  ncol = 3)
+png("rex_abiotic.png", units="in", width=12, height=5, res=300)
+annotate_figure(abiotic_comb,
+                bottom = text_grob("Treatment", color = "black", size=17))
 dev.off()
 
 # plot - monthly lux
